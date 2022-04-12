a72ab60e0cc9ad27372e6fe7683a46ee35e26a1b
codervivek496 <viveksinghssm496@gmail.com>
2022-04-12 15:35:49 +0530

    empwageuc1

--- empwage.sh
+++ empwage.sh
@@ -1,1 +1,11 @@
 #Welcome to Employee Wage Computation program on Master Branch
+
+#! /bin/bash -x
+present=1;
+r=$(( RANDOM%2 ));
+if [ $(( pesent )) -eq $r ];
+then
+	echo "Employee is present";
+else
+	echo "Employee is absent";
+fi
