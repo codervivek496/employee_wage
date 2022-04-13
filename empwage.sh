f606f9fec12b911a7709a6ba5c7aa63681f97884
codervivek496 <viveksinghssm496@gmail.com>
2022-04-13 11:17:28 +0530

    uc3

--- empwage.sh
+++ empwage.sh
@@ -1,15 +1,22 @@
 #Welcome to Employee Wage Computation program on Master Branch
 
 #! /bin/bash -x
-present=1;
-r=$(( RANDOM%2 ));
-if [ $(( pesent )) -eq $r ];
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+r=$(( RANDOM%3 ));
+if [ $(( isPartTime )) -eq $r ];
 then
-	echo "Employee is present";
-	empRatePerHr=20;
+	echo "Employee is Part Time";
+	empHrs=4;
+elif [ $(( $isFullTime )) -eq $r ];
+then
+	echo "Employee is Full Time"
 	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr))
 else
 	echo "Employee is absent";
-	salary=0;
+	empHrs=0;
 fi
+salary=$(($empHrs*$empRatePerHr))
+echo "Employee working hours is $empHrs"
+echo "Employee salary is $salary"
