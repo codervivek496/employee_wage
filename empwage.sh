5043ad26b73126eb63d4d7e0bb84c8a39881751e
codervivek496 <viveksinghssm496@gmail.com>
2022-04-13 08:41:34 +0530

    empwageuc2

--- empwage.sh
+++ empwage.sh
@@ -6,6 +6,10 @@ r=$(( RANDOM%2 ));
 if [ $(( pesent )) -eq $r ];
 then
 	echo "Employee is present";
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr))
 else
 	echo "Employee is absent";
+	salary=0;
 fi
