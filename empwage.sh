f3a0bdb4d249b19dee7518b425650159087a3d93
codervivek496 <viveksinghssm496@gmail.com>
2022-04-14 09:40:16 +0530

    empwage_Uc5

--- empwage.sh
+++ empwage.sh
@@ -1,21 +1,25 @@
 #Welcome to Employee Wage Computation program on Master Branch
 
-#! /bin/bash -x
+
 isPartTime=1;
 isFullTime=2;
+totalsalary=0;
 empRatePerHr=20;
-r=$(( RANDOM%3 ));
-case $r in
-	1)
-	echo "Employee is Part Time"
-	empHrs=4;;
-	2)
-	echo "Employee is Full Time"
-	empHrs=8;;
-	*)
-	echo "Employee is absent"
-	empHrs=0;;
-esac
-salary=$(($empHrs*$empRatePerHr))
-echo "Employee working hours is $empHrs"
-echo "Employee salary is $salary"
+numWorkingDays=20;
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	r=$(( RANDOM%3 ))
+		case $r in
+			1)
+			empHrs=4;;
+			2)
+			empHrs=8;;
+			*)
+			empHrs=0;;
+		esac
+salary=$(( $empHrs*$empRatePerHr ))
+totalSalary=$(( $totalSalary + $salary ))
+done
+
+echo $totalSalary
